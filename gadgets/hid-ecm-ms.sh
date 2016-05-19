372ef6722cb4224f91ec261e2c6b1bd7a62848e4
Quentin Young <qlyoung@qlyoung.net>
2016-05-19 01:17:25 -0400

    hid-ecm-ms.sh now keeps the same MAC as the debian jessie image

--- gadgets/hid-ecm-ms.sh
+++ gadgets/hid-ecm-ms.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 #
-# Sets up a composite USB device providing Ethernet over USB, HID, and
-# Mass Storage capabilities. Code from various unlicensed sources.
+# Sets up a composite HID + ECM + Mass Storage USB device.
 #
+# Expects to be run on the Debian Jessie base image
+# provided by Inverse Path.
+#
+# Usage:
+#   # ./hid-ecm-ms.sh <path-to-img>
+#
+# <path-to-img> must be the full path to a disk image file.
+#
+# The easiest way to make this disk image file is to create
+# a blank file of the desired size, set up the USBArmory as
+# a mass storage device (e.g. with this script) and then use
+# the host to format and partition the mass storage device
+# exposed by the USBArmory as desired.
+# Example execution:
+#
+#   $ dd if=/dev/zero of=disk.img bs=1M count=1024
+#   # ./hid-ecm-ms.sh /home/usbarmory/disk.img
+#
+# Then on the host computer you'll see a new block device,
+# e.g. /dev/sdb, and from there you can use fdisk or gparted
+# or what have you to create a partition table, partitions,
+# and a filesystem.
+#
+# The MAC address will be set to the same one
+# that the Debian Jessie base image uses, namely
+# 1a:55:89:a2:69:41. The host will see the device
+# as having the MAC address 1a:55:89:a2:69:42.
+#
+# Author:
+#   Quentin Young
+#   <unknown>
 
 # load libcomposite
 modprobe libcomposite
@@ -10,34 +40,38 @@ modprobe libcomposite
 # remove all USB Ethernet drivers
 modprobe -r g_ether usb_f_ecm u_ether
 
-# insert FunctionFS modules for HID, ECM Ethernet, and Mass Storage
-# these will provide functions for our USB gadget
+# insert modules for HID, ECM Ethernet, and Mass Storage
 modprobe usb_f_hid
 modprobe usb_f_ecm
 modprobe usb_f_mass_storage
 
-# instantiate a new USB gadget via ConfigFS and cd to it
+# make new usb gadget in configfs
 cd /sys/kernel/config/
 mkdir usb_gadget/g1
 cd usb_gadget/g1
-
-# create a new configuration for this gadget and add three functions (HID, ECM, MS)
 mkdir configs/c.1
+
+# enable hid, ecm and mass storage functions
 mkdir functions/hid.usb0
 mkdir functions/ecm.usb0
 mkdir functions/mass_storage.ms0
 
-# configure mass storage function by setting path to image to expose as MS filesystem
-echo /massstorage.img > functions/mass_storage.ms0/lun.0/file
-echo 1 > functions/mass_storage.ms0/lun.0/removable
-
-# configure HID Keyboard; see USB HID Class Specification for details
+# setup hid parameters for the keyboard function
 echo 1 > functions/hid.usb0/protocol
 echo 1 > functions/hid.usb0/subclass
 echo 8 > functions/hid.usb0/report_length
 echo -ne "\x05\x01\x09\x06\xA1\x01\x05\x07\x19\xE0\x29\xE7\x15\x00\x25\x01\x75\x01\x95\x08\x81\x02\x95\x01\x75\x08\x81\x03\x95\x05\x75\x01\x05\x08\x19\x01\x29\x05\x91\x02\x95\x01\x75\x03\x91\x03\x95\x06\x75\x08\x15\x00\x25\x65\x05\x07\x19\x00\x29\x65\x81\x00\xC0" > functions/hid.usb0/report_desc
 
-# configure general properties for this USB device; see USB Specification for details
+# set the host and device MAC addresses to be the same as the armory's
+# default Debian Jessie MAC address as set in /etc/modprobe/usbarmory.conf
+echo 1a:55:89:a2:69:41 > functions/ecm.usb0/dev_addr
+echo 1a:55:89:a2:69:42 > functions/ecm.usb0/host_addr
+
+# configure mass storage function by setting path to image to expose as the disk device
+echo $1 > functions/mass_storage.ms0/lun.0/file
+echo 1 > functions/mass_storage.ms0/lun.0/removable
+
+# configure general properties for the USB device
 mkdir strings/0x409
 mkdir configs/c.1/strings/0x409
 echo 0x1d6b > idVendor                  # Linux Foundation
@@ -52,11 +86,10 @@ echo "USBArmory Composite Device" > strings/0x409/product
 echo "Conf1" > configs/c.1/strings/0x409/configuration
 echo 120 > configs/c.1/MaxPower
 
-
-# bind all functions provided by FunctionFS to our configuration
+# link and enable functionfs functions
 ln -s functions/hid.usb0 configs/c.1/
 ln -s functions/ecm.usb0 configs/c.1/
 ln -s functions/mass_storage.ms0 configs/c.1/
 
-# bind USBG driver
+# bind usbg driver
 echo ci_hdrc.0 > UDC
